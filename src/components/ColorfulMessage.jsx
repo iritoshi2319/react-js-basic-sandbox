d20421ac3254c3f19f67f49dad28d803bb194fd0
Toshihiko Irishio <72386294+iritoshi2319@users.noreply.github.com>
2023-10-17 21:28:23 +0900

    Initial commit

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ColorfulMessage = (props) => {
+  // props.colorとかprops.childrenとか毎回propsて書くの面倒なので分割代入しておく
+  const { color, children } = props;
+
+  const contentStyle = {
+    color: color,
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}>{children}</p>;
+};
+
+export default ColorfulMessage;
